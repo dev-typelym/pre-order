78bd73ab0d9492b4b9ea1719aa18fa42c3bf60de
dev-typelym <dlstk3031@gmail.com>
2025-06-15 20:44:27 +0900

    refactor: Cart 도메인 책임 강화 및 QueryDSL 제거
    - Cart 내부에 addOrUpdateItem, decreaseItem, deleteItemsByIds 도메인 메서드 추가
    - CartServiceImpl에서 cart.getCartItems().add/remove 등 직접 조작 로직 제거
    - CartQueryDsl 및 구현 클래스 제거, Spring Data JPA 메서드 findCartByMemberId(Long)로 대체

--- cart-service/src/main/java/com/app/preorder/cartservice/repository/cart/CartQueryDsl.java
+++ cart-service/src/main/java/com/app/preorder/cartservice/repository/cart/CartQueryDsl.java
@@ -3,8 +3,8 @@ package com.app.preorder.cartservice.repository.cart;
 
 import com.app.preorder.cartservice.domain.entity.Cart;
 
+import java.util.Optional;
+
 public interface CartQueryDsl {
 
-    // 멤버 아이디로 카트 찾기
-    Cart findCartByMemberId(Long memberId);
 }
--- cart-service/src/main/java/com/app/preorder/cartservice/repository/cart/CartRepository.java
+++ cart-service/src/main/java/com/app/preorder/cartservice/repository/cart/CartRepository.java
@@ -4,6 +4,10 @@ package com.app.preorder.cartservice.repository.cart;
 import com.app.preorder.cartservice.domain.entity.Cart;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CartRepository extends JpaRepository<Cart, Long>, CartQueryDsl {
 
+    // 멤버 아이디로 카트 찾기
+    Optional<Cart> findCartByMemberId(Long memberId);
 }
